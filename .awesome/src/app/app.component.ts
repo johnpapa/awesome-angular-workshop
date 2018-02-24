8f0bd4151fb7ad6b85922649a0dbdb3e736d0e74
John L Papa <john@johnpapa.net>
2018-02-24 16:10:11 -0500

    init

---
+++ .awesome/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'aw-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'aw';
+}

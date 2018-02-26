04b2e3c34a819a3f97f7e15d95b016752df5f335
John L Papa <john@johnpapa.net>
2018-02-25 19:41:24 -0500

    added routing begin - init

---
+++ src/client/1-routing-begin/core/toolbar/toolbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'aw-toolbar',
+  templateUrl: './toolbar.component.html',
+  styleUrls: ['./toolbar.component.scss']
+})
+export class ToolbarComponent {
+  labTitle = '0-awesome';
+  labState = 'template';
+}

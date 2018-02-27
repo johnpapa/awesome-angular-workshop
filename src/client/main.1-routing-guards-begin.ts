0652cfb05bc9ca004bdb2525b23ad917d7df20c4
John L Papa <john@johnpapa.net>
2018-02-27 23:15:35 +0200

    route guards

---
+++ src/client/main.1-routing-guards-begin.ts
@@ -0,0 +1,14 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './1-routing-guards-begin/app.module';
+import { AppDevModule } from './1-routing-guards-begin/app-dev.module';
+import { environment } from './environments/environment';
+
+import 'hammerjs';
+
+if (environment.production) {
+  enableProdMode();
+}
+platformBrowserDynamic().bootstrapModule(AppDevModule);
+// platformBrowserDynamic().bootstrapModule(AppModule);

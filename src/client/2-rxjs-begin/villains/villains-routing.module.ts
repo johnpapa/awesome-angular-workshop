1e4cd31fc2fb2a18c69e2d14f45793c412aab74c
Ward Bell <wardbell@hotmail.com>
2018-02-26 17:46:56 -0800

    Add 2-rxjs-begin

---
+++ src/client/2-rxjs-begin/villains/villains-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { VillainsComponent } from './villains/villains.component';
+import { VillainDetailContainerComponent } from './villain-detail-container/villain-detail-container.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: VillainsComponent,
+    children: [{ path: 'details/:id', component: VillainDetailContainerComponent }]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class VillainsRoutingModule {}

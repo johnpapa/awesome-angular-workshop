873631b44de5d909d7fade4d45844a57a190697b
John Papa <johnpapa@users.noreply.github.com>
2018-02-28 00:50:36 +0200

    Deploy (#9)
    * route guard fix
    
    * deploy begin
    
    * deploy begin
    
    * deploy end

--- src/server/index.js
+++ src/server/index.js
@@ -1,28 +1,11 @@
-if (!process.env.NODE_ENV) {
-  console.error(
-    'ENV variables are missing.',
-    'Verify that you have set them directly or in a .env file.'
-  );
-  process.exit(1);
-}
-
 const express = require('express');
-const bodyParser = require('body-parser');
-const router = express.Router();
-const routes = require('./routes');
-
-const publicweb = process.env.PUBLICWEB;
-const port = process.env.PORT;
-
 const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
+const port = process.env.PORT || 9070;
+const publicweb = process.env.PUBLICWEB || './dist';
 app.use(express.static(publicweb));
 console.log(`serving ${publicweb}`);
-app.use('/api', routes);
 app.get('*', (req, res) => {
   res.sendFile(`index.html`, { root: publicweb });
 });
+app.listen(port, () => console.log(`listening on http://localhost:${port}`));
 
-app.listen(port, () => console.log(`API running on http://localhost:${port}`));

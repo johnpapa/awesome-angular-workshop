01343b9694e17e2c3510e199e383a2d241fc8c63
John L Papa <john@johnpapa.net>
2018-06-07 01:26:27 +0200

    fixes for types

--- src/client/6-ngrx-data-begin/store-old/entity-metadata.ts
+++ src/client/6-ngrx-data-begin/store-old/entity-metadata.ts
@@ -1,7 +1,32 @@
-import { defaultSelectId, EntityMetadataMap, PropsFilterFnFactory } from 'ngrx-data';
+import {
+  defaultSelectId,
+  EntityMetadataMap,
+  PropsFilterFnFactory
+} from 'ngrx-data';
 
 import { Hero, Villain } from '../core/model';
 
+export const entityMetadata: EntityMetadataMap = {
+  Hero: {
+    filterFn: nameFilter, // optional
+    sortComparer: sortByName // optional
+  },
+
+  Villain: {
+    entityName: 'Villain', // optional because same as map key
+    selectId: villainSelectId, //  a non-default function
+    filterFn: nameAndSayingFilter, // optional
+    // Optional: overrides certain default dispatcher behaviors
+    entityDispatcherOptions: { optimisticAdd: true, optimisticUpdate: true }
+  }
+};
+
+export const pluralNames = {
+  // Case matters. Match the case of the entity name.
+  Hero: 'Heroes'
+};
+
+///////////
 /////////
 // AOT obliges us to encapsulate the logic in wrapper functions
 export function sortByName(a: { name: string }, b: { name: string }): number {
@@ -18,33 +43,18 @@ export function villainSelectId<T extends { id: any }>(entity: T) {
 }
 
 /** Filter for entities whose name matches the case-insensitive pattern */
-export function nameFilter<T>(entities: T[], pattern: string) {
+export function nameFilter<T extends { name: string }>(
+  entities: T[],
+  pattern: string
+) {
   return PropsFilterFnFactory(['name'])(entities, pattern);
 }
 
 /** Filter for entities whose name or saying matches the case-insensitive pattern */
-export function nameAndSayingFilter<T>(entities: T[], pattern: string) {
+export function nameAndSayingFilter<T extends { name: string; saying: string }>(
+  entities: T[],
+  pattern: string
+) {
   return PropsFilterFnFactory(['name', 'saying'])(entities, pattern);
 }
 ////////////
-
-export const entityMetadata: EntityMetadataMap = {
-
-  Hero: {
-    filterFn: nameFilter,    // optional
-    sortComparer: sortByName // optional
-  },
-
-  Villain: {
-    entityName: 'Villain', // optional because same as map key
-    selectId: villainSelectId,  //  a non-default function
-    filterFn: nameAndSayingFilter, // optional
-    // Optional: overrides certain default dispatcher behaviors
-    entityDispatcherOptions: { optimisticAdd: true, optimisticUpdate: true }
-  }
-};
-
-export const pluralNames = {
-  // Case matters. Match the case of the entity name.
-  Hero: 'Heroes'
-};
